ee1b95fc47390eb6aa02f7e6130bbe223cc078da
pagidalasaikiran <pagidala.saikiran@gmail.com>
2025-06-04 20:25:06 +0530

    Add files via upload

---
+++ extract_features.py
@@ -0,0 +1,7 @@
+import librosa
+import numpy as np
+
+def extract_features(file_path):
+    audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast')
+    mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    return np.mean(mfccs.T, axis=0)
